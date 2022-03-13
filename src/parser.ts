42cc9506806ab2abb33cfef157821029d56e658f
myuon <ioi.joi.koi.loi@gmail.com>
2022-03-13 19:09:13 +0900

    fix: types

--- src/parser.ts
+++ src/parser.ts
@@ -1,23 +1,24 @@
 import * as csstree from "css-tree";
 
-const Ampersand = 0x0026;
+export const extended: typeof csstree = (csstree as any).fork(
+  (syntaxConfig: any) => {
+    const defaultGetNode = syntaxConfig.scope.Selector.getNode;
 
-export const extended = csstree.fork((syntaxConfig) => {
-  const defaultGetNode = syntaxConfig.scope.Selector.getNode;
+    syntaxConfig.scope.Selector.getNode = function (context: any) {
+      // 0x0026: &
+      if (this.isDelim(0x0026)) {
+        const start = this.tokenStart;
+        this.next();
 
-  syntaxConfig.scope.Selector.getNode = function (context) {
-    if (this.isDelim(Ampersand)) {
-      const start = this.tokenStart;
-      this.next();
+        return {
+          type: "Nested",
+          loc: this.getLocation(start, this.tokenEnd),
+        };
+      }
 
-      return {
-        type: "Nested",
-        loc: this.getLocation(start, this.tokenEnd),
-      };
-    }
+      return defaultGetNode.call(this, context);
+    };
 
-    return defaultGetNode.call(this, context);
-  };
-
-  return syntaxConfig;
-});
+    return syntaxConfig;
+  }
+);
