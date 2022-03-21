005b8134937054e3a7bc7fdcabdfe5df3d71a2ed
myuon <ioi.joi.koi.loi@gmail.com>
2022-03-21 19:06:48 +0900

    fix: just try-catch everything

--- src/rules/property-reorder.ts
+++ src/rules/property-reorder.ts
@@ -21,67 +21,71 @@ export const propertyReorder: TSESLint.RuleModule<"property-reorder", []> = {
       "TaggedTemplateExpression[tag.name='css']": (
         eslintNode: TSESTree.TaggedTemplateExpression
       ) => {
-        // UGLY!
-        const raw = `&{${eslintNode.quasi.quasis.reduce(
-          (acc, cur, index) =>
-            acc +
-            (index > 0 ? `var(--placeholder_${index - 1})` : "") +
-            cur.value.raw,
-          ""
-        )}}`;
-        const root = parse(raw);
+        try {
+          // UGLY!
+          const raw = `&{${eslintNode.quasi.quasis.reduce(
+            (acc, cur, index) =>
+              acc +
+              (index > 0 ? `var(--placeholder_${index - 1})` : "") +
+              cur.value.raw,
+            ""
+          )}}`;
+          const root = parse(raw);
 
-        const reorder = (input: Node) => {
-          const node = getContainingNode(input);
+          const reorder = (input: Node) => {
+            const node = getContainingNode(input);
 
-          sortNode(node, [
-            "custom-properties",
-            "dollar-variables",
-            "at-variables",
-            "declarations",
-            "rules",
-            "at-rules",
-          ]);
-          sortNodeProperties(node, {
-            order: properties,
-            unspecifiedPropertiesPosition: "bottom",
-          });
-        };
-
-        const rootChanged = root.clone();
-        rootChanged.walk(reorder);
-
-        if (root.toString() !== rootChanged.toString()) {
-          // You can't just stringify rootChanged.first, need to strip the enclosing {}
-          const re = /\&\{(.*)\}/s;
-          const body = rootChanged.toString().match(re)?.[1].trim();
-          if (!body) {
-            throw new Error("Unexpected");
-          }
-
-          // Recover placeholder_**
-          const source = context.getSourceCode();
-          const recover = (body: string) => {
-            return eslintNode.quasi.expressions.reduce(
-              (acc, expr, index) =>
-                acc.replace(
-                  `var(--placeholder_${index})`,
-                  "${" + `${source.getText(expr)}` + "}"
-                ),
-              body
-            );
+            sortNode(node, [
+              "custom-properties",
+              "dollar-variables",
+              "at-variables",
+              "declarations",
+              "rules",
+              "at-rules",
+            ]);
+            sortNodeProperties(node, {
+              order: properties,
+              unspecifiedPropertiesPosition: "bottom",
+            });
           };
 
-          context.report({
-            node: eslintNode,
-            messageId: "property-reorder",
-            fix: (fixer) => {
-              return fixer.replaceText(
-                eslintNode,
-                "css`" + recover(body) + "`"
+          const rootChanged = root.clone();
+          rootChanged.walk(reorder);
+
+          if (root.toString() !== rootChanged.toString()) {
+            // You can't just stringify rootChanged.first, need to strip the enclosing {}
+            const re = /\&\{(.*)\}/s;
+            const body = rootChanged.toString().match(re)?.[1].trim();
+            if (!body) {
+              throw new Error("Unexpected");
+            }
+
+            // Recover placeholder_**
+            const source = context.getSourceCode();
+            const recover = (body: string) => {
+              return eslintNode.quasi.expressions.reduce(
+                (acc, expr, index) =>
+                  acc.replace(
+                    `var(--placeholder_${index})`,
+                    "${" + `${source.getText(expr)}` + "}"
+                  ),
+                body
               );
-            },
-          });
+            };
+
+            context.report({
+              node: eslintNode,
+              messageId: "property-reorder",
+              fix: (fixer) => {
+                return fixer.replaceText(
+                  eslintNode,
+                  "css`" + recover(body) + "`"
+                );
+              },
+            });
+          }
+        } catch (err) {
+          console.error(`[css-reorder/property-reoreder]: ${err}`);
         }
       },
     };
